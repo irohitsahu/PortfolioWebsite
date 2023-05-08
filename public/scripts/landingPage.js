c28b55ba1bdce4689951dcf78aaf931e07806c26
Rohit Sahu <irohitsahu97@gmail.com>
2023-05-08 15:01:33 +0530

    Added Button Effects

---
+++ public/scripts/landingPage.js
@@ -0,0 +1,67 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+// -------------------------------
+window.onscroll = function () { myFunction() };
+
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky")
+    }
+}
+
+// ---------------------------------
+
+function openForm(x) {
+
+    const tabOpen = document.getElementsByClassName("tab-links");
+    if (x === 'projects') {
+        if (document.getElementById(x).style.display === "block") {
+            document.getElementById(x).style.display = "none"
+            tabOpen[0].classList.remove("tab-links-focus")
+        }
+        else {
+            document.getElementById(x).style.display = "block"
+            document.getElementById('skills').style.display = "none"
+            document.getElementById('education').style.display = "none"
+            tabOpen[0].classList.add("tab-links-focus")
+            tabOpen[2].classList.remove("tab-links-focus")
+            tabOpen[1].classList.remove("tab-links-focus")
+        }
+    }
+    if (x === 'skills') {
+        if (document.getElementById(x).style.display === "block") {
+            document.getElementById(x).style.display = "none"
+            tabOpen[1].classList.remove("tab-links-focus")
+        }
+        else {
+            document.getElementById(x).style.display = "block"
+            document.getElementById('education').style.display = "none"
+            document.getElementById('projects').style.display = "none"
+            tabOpen[1].classList.add("tab-links-focus")
+            tabOpen[0].classList.remove("tab-links-focus")
+            tabOpen[2].classList.remove("tab-links-focus")
+        }
+
+    }
+    if (x === 'education') {
+        if (document.getElementById(x).style.display === "block") {
+            document.getElementById(x).style.display = "none"
+            tabOpen[2].classList.remove("tab-links-focus")
+        }
+
+        else {
+            document.getElementById(x).style.display = "block"
+            document.getElementById('skills').style.display = "none"
+            document.getElementById('projects').style.display = "none"
+            tabOpen[2].classList.add("tab-links-focus")
+            tabOpen[0].classList.remove("tab-links-focus")
+            tabOpen[1].classList.remove("tab-links-focus")
+        }
+    }
+}
